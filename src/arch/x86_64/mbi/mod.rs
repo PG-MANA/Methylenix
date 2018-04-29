48effde04897ba2665799981e9da3ce2d69cdbfa
PG_MANA <pg_mana@taprix.org>
2018-04-29 19:35:42 +0900

    [clean] mbi/mod.rsのコード整理
    Multi Boot Informationの部分において一部関連定数化

--- src/arch/x86_64/mbi/mod.rs
+++ src/arch/x86_64/mbi/mod.rs
@@ -7,44 +7,11 @@
  * MultiBoot2実装
  */
 
-//参考:http://git.savannah.gnu.org/cgit/grub.git/tree/doc/kernel.c?h=multiboot2
+//参考:https://www.gnu.org/software/grub/manual/multiboot2/multiboot.html
 
-//マルチブートインフォメーションに関する宣言
-//enum => u32が難しそう
-//enum TagType{
-//#defineに当たる
-
-#![allow(dead_code)]
-//!をつけているのでカッコがないこの場合はグローバル適用
-const MULTIBOOT_TAG_ALIGN: u32 = 8;
-const MULTIBOOT_TAG_TYPE_END: u32 = 0;
-const MULTIBOOT_TAG_TYPE_CMDLINE: u32 = 1;
-const MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME: u32 = 2;
-const MULTIBOOT_TAG_TYPE_MODULE: u32 = 3;
-const MULTIBOOT_TAG_TYPE_BASIC_MEMINFO: u32 = 4;
-const MULTIBOOT_TAG_TYPE_BOOTDEV: u32 = 5;
-const MULTIBOOT_TAG_TYPE_MMAP: u32 = 6;
-const MULTIBOOT_TAG_TYPE_VBE: u32 = 7;
-const MULTIBOOT_TAG_TYPE_FRAMEBUFFER: u32 = 8;
-const MULTIBOOT_TAG_TYPE_ELF_SECTIONS: u32 = 9;
-const MULTIBOOT_TAG_TYPE_APM: u32 = 10;
-const MULTIBOOT_TAG_TYPE_EFI32: u32 = 11;
-const MULTIBOOT_TAG_TYPE_EFI64: u32 = 12;
-const MULTIBOOT_TAG_TYPE_SMBIOS: u32 = 13;
-const MULTIBOOT_TAG_TYPE_ACPI_OLD: u32 = 14;
-const MULTIBOOT_TAG_TYPE_ACPI_NEW: u32 = 15;
-const MULTIBOOT_TAG_TYPE_NETWORK: u32 = 16;
-const MULTIBOOT_TAG_TYPE_EFI_MMAP: u32 = 17;
-const MULTIBOOT_TAG_TYPE_EFI_BS: u32 = 18;
-const MULTIBOOT_TAG_TYPE_EFI32_IH: u32 = 19;
-const MULTIBOOT_TAG_TYPE_EFI64_IH: u32 = 20;
-const MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR: u32 = 21;
-
-//Module
 mod elf;
 mod memory;
 
-//use
 pub use self::elf::{ElfInfo, ElfSection};
 pub use self::memory::{MemoryInfo, MemoryMapEntry, MemoryMapInfo};
 use core::mem;
@@ -63,6 +30,31 @@ pub struct MultiBootInformation {
 }
 
 impl MultiBootInformation {
+    #![allow(dead_code)]//使っていない定数でエラーが出る
+    const TAG_ALIGN: u32 = 8;
+    const TAG_TYPE_END: u32 = 0;
+    const TAG_TYPE_CMDLINE: u32 = 1;
+    const TAG_TYPE_BOOT_LOADER_NAME: u32 = 2;
+    const TAG_TYPE_MODULE: u32 = 3;
+    const TAG_TYPE_BASIC_MEMINFO: u32 = 4;
+    const TAG_TYPE_BOOTDEV: u32 = 5;
+    const TAG_TYPE_MMAP: u32 = 6;
+    const TAG_TYPE_VBE: u32 = 7;
+    const TAG_TYPE_FRAMEBUFFER: u32 = 8;
+    const TAG_TYPE_ELF_SECTIONS: u32 = 9;
+    const TAG_TYPE_APM: u32 = 10;
+    const TAG_TYPE_EFI32: u32 = 11;
+    const TAG_TYPE_EFI64: u32 = 12;
+    const TAG_TYPE_SMBIOS: u32 = 13;
+    const TAG_TYPE_ACPI_OLD: u32 = 14;
+    const TAG_TYPE_ACPI_NEW: u32 = 15;
+    const TAG_TYPE_NETWORK: u32 = 16;
+    const TAG_TYPE_EFI_MMAP: u32 = 17;
+    const TAG_TYPE_EFI_BS: u32 = 18;
+    const TAG_TYPE_EFI32_IH: u32 = 19;
+    const TAG_TYPE_EFI64_IH: u32 = 20;
+    const TAG_TYPE_LOAD_BASE_ADDR: u32 = 21;
+
     pub fn new(addr: usize) -> MultiBootInformation {
         //core::mem::uninitializedは慎重に使うべき(zeroedも避けるべきだがデフォルトの数値がすべて0だから...)
         //let mut mbi = MultiBootInformation{..Default::default()};
@@ -72,49 +64,49 @@ impl MultiBootInformation {
         let mut tag = addr + 8;
 
         loop {
-            let typ: u32 = unsafe { (&*(tag as *const MultibootTag)).s_type };
-            match typ {
-                MULTIBOOT_TAG_TYPE_END => {
+            let tag_type: u32 = unsafe { (&*(tag as *const MultibootTag)).s_type };
+            match tag_type {
+                MultiBootInformation::TAG_TYPE_END => {
                     puts!("END\n");
                     break;
                 }
-                MULTIBOOT_TAG_TYPE_MMAP => {
+                MultiBootInformation::TAG_TYPE_MMAP => {
                     puts!("Memory Map=>");
                     mbi.memmapinfo = MemoryMapInfo::new(unsafe { &*(tag as *const _) });
                 }
-                MULTIBOOT_TAG_TYPE_ACPI_NEW => {
+                MultiBootInformation::TAG_TYPE_ACPI_NEW => {
                     puts!("ACPI(NEW)=>");
                 }
-                MULTIBOOT_TAG_TYPE_BASIC_MEMINFO => {
+                MultiBootInformation::TAG_TYPE_BASIC_MEMINFO => {
                     puts!("Basic MemoryInfo=>");
                     mbi.meminfo = MemoryInfo::new(unsafe { &*(tag as *const _) }); //完全に信用すべきではない(CPUIDなどで問い合わせる)
                 }
-                MULTIBOOT_TAG_TYPE_VBE => {
+                MultiBootInformation::TAG_TYPE_VBE => {
                     puts!("VBE Struct=>");
                 }
-                MULTIBOOT_TAG_TYPE_CMDLINE => {
+                MultiBootInformation::TAG_TYPE_CMDLINE => {
                     puts!("Cmdline=>");
                 }
-                MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME => {
+                MultiBootInformation::TAG_TYPE_BOOT_LOADER_NAME => {
                     puts!("Boot Loader Name=>");
                 }
-                MULTIBOOT_TAG_TYPE_SMBIOS => {
+                MultiBootInformation::TAG_TYPE_SMBIOS => {
                     puts!("SMBIOS=>");
                 }
-                MULTIBOOT_TAG_TYPE_FRAMEBUFFER => {
+                MultiBootInformation::TAG_TYPE_FRAMEBUFFER => {
                     puts!("FRAMEBUFFER=>");
                 }
-                MULTIBOOT_TAG_TYPE_EFI32 => {
+                MultiBootInformation::TAG_TYPE_EFI32 => {
                     puts!("EFI32=>");
                 }
-                MULTIBOOT_TAG_TYPE_ELF_SECTIONS => {
+                MultiBootInformation::TAG_TYPE_ELF_SECTIONS => {
                     mbi.elfinfo = ElfInfo::new(unsafe { &*(tag as *const _) });
                     puts!("ELF SECTIONS=>");
                 }
-                MULTIBOOT_TAG_TYPE_APM => {
+                MultiBootInformation::TAG_TYPE_APM => {
                     puts!("APM=>");
                 }
-                MULTIBOOT_TAG_TYPE_BOOTDEV => {
+                MultiBootInformation::TAG_TYPE_BOOTDEV => {
                     puts!("BOOTDEV=>");
                 }
                 _ => {
@@ -132,17 +124,6 @@ impl MultiBootInformation {
         mbi
     }
 }
-
-/*
-impl Default for MultiBootInformation{
-    fn default() -> MultiBootInformation {//デフォルト値
-        MultiBootInformation{
-            meminfo : MemoryInfo{..Default::default()},
-            elfinfo : ElfInfo{..Default::default()},
-            memmapinfo : MemoryMapInfo{..Default::default()},
-        }
-    }
-}*/
 
 pub fn test(addr: usize) -> bool {
     if addr & 7 != 0
