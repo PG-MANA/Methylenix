051425be3aad36c0184f59410ac9ef2f55ed8819
PG_MANA <pg_mana@taprix.org>
2018-10-27 21:18:56 +0900

    [clean] format assembly codes
    タブの長さなどを2に変更し、インデントを統一した。
    わかりにくい名前を変更した。
    noid => nocpuid

--- src/arch/x86_64/boot/grub.asm
+++ src/arch/x86_64/boot/grub.asm
@@ -1,123 +1,121 @@
 ;GRUB2で起動できたらなと作ったもの。(ELF式)
-;
 bits    32
 
 global  boot_entry
 global  stack_top
-extern  init;init.asm
+extern  init  ; init.asm
 
-;grub2のためのエントリー(multiboot2仕様)
+; grub2のためのエントリー(multiboot2仕様)
 ; http://git.savannah.gnu.org/cgit/grub.git/tree/doc/multiboot2.h?h=multiboot2
-multiboot_header_magic          equ 0xe85250d6 ; 合言葉
-multiboot_header_arch           equ 0;4ならmips
-multiboot_header_len            equ multiboot_end - multiboot_start;ヘッダの長さ
-multiboot_header_checksum       equ 0x100000000-(multiboot_header_magic + multiboot_header_arch + multiboot_header_len)
-multiboot_bootloader_magic      equ 0x36d76289
+MULTIBOOT_HEADER_MAGIC          equ 0xe85250d6  ; 合言葉
+MULTIBOOT_HEADER_ARCH           equ 0           ; 4ならmips
+MULTIBOOT_HEADER_LEN            equ multiboot_end - multiboot_start
+MULTIBOOT_HEADER_CHECKSUM       equ 0x100000000-(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_ARCH + MULTIBOOT_HEADER_LEN)
+MULTIBOOT_CHECK_MAGIC           equ 0x36d76289  ; 正常に処理されたのであれば、EAXに代入されている値
+MULTIBOOT_HEADER_TAG_END        equ 0           ; マルチブート用ヘッダー 定数定義終了(実体は下)
 
-
-;grub2のタグ
-
-multiboot_header_tag_end    equ 0;終了
-
-
-;その他定数
-stack_size   equ 256;初期のスタックサイズ(64bit化後変更になる)
-
-;end
+; その他定数
+stack_size                      equ 256         ; 初期のスタックサイズ(64bit化後変更になる)
 ;========================================
 
 
 ;========================================
-;マルチブート用ヘッダー
-jmp     boot_entry;下を実行されたらまずいのでjmp
-section .grub_header ;特殊な扱い
-align   4;32bit
-multiboot_start:
-    dd  multiboot_header_magic
-    dd  multiboot_header_arch
-    dd  multiboot_header_len
-    dd  multiboot_header_checksum
+; マルチブート用ヘッダー
+section .grub_header  ; 特殊な扱いのセクションにする(配置固定 & 最適化無効)
 
-;ここにタグを書く
-multiboot_tag_end:
-    dw  multiboot_header_tag_end
-    dw  0;flags
-    dd  8;size
-multiboot_end:
-;マルチブート用ヘッダー記述終了
-;========================================
+jmp     boot_entry    ; 下を実行されたらまずいのでjmp
 
-
-;========================================
-;textセクション
-section .text
 align   4
-boot_entry:;起動処理のキモ
-mov     esp,stack_top;スタック設定
-;eflags初期化
-push    0
-popf
-push    0;64bit popのための準備
-push    ebx;アドレス保存
 
-;eaxとebxはいじってはいけない
-cmp     eax,multiboot_bootloader_magic
-jne     bad_magic
-jmp     init;初期化
+multiboot_start:
+  dd  MULTIBOOT_HEADER_MAGIC
+  dd  MULTIBOOT_HEADER_ARCH
+  dd  MULTIBOOT_HEADER_LEN
+  dd  MULTIBOOT_HEADER_CHECKSUM
+
+; ここに追加のタグを書く
+multiboot_tag_end:
+  dw  MULTIBOOT_HEADER_TAG_END
+  dw  0 ; flags
+  dd  8 ; size
+multiboot_end:
+; マルチブート用ヘッダー実体記述終了
+;========================================
+
+
+;========================================
+section .text
+align 4
+boot_entry:           ; 起動処理のキモ
+  mov   esp, stack_top ; スタック設定
+
+  ; eflags初期化
+  push  0
+  popf
+  push  0             ; 64bit popのための準備
+  push  ebx           ; アドレス保存
+
+  ; eaxとebxはいじってはいけない
+  cmp   eax, MULTIBOOT_CHECK_MAGIC
+  jne   bad_magic
+  jmp   init          ; 初期化( init.asmへ )
 
 bad_magic:
-    mov ecx,error_str.end-error_str
-    mov edi,0xb8000
-    mov esi,error_str
-    rep movsb;転送
-    cli
-    hlt
+  mov   ecx, error_str.end-error_str
+  mov   edi, 0xb8000
+  mov   esi, error_str
+  rep   movsb         ; 転送
+  cli
+  hlt
 jmp bad_magic
 
 
 section .data
+
 align   4
+
 error_str:
-    ;dw 0x4f52と書くがエンディアンで分けるときは逆に書く
-    db  'E',0x4f;4f:赤字に白文字
-    db  'r',0x4f
-    db  'r',0x4f
-    db  'o',0x4f
-    db  'r',0x4f
-    db  ':',0x4f
-    db  'M',0x4f
-    db  'u',0x4f
-    db  'l',0x4f
-    db  't',0x4f
-    db  'i',0x4f
-    db  'b',0x4f
-    db  'o',0x4f
-    db  'o',0x4f
-    db  't',0x4f
-    db  ' ',0x4f
-    db  'i',0x4f
-    db  's',0x4f
-    db  ' ',0x4f
-    db  'n',0x4f
-    db  'o',0x4f
-    db  't',0x4f
-    db  ' ',0x4f
-    db  's',0x4f
-    db  'u',0x4f
-    db  'p',0x4f
-    db  'p',0x4f
-    db  'o',0x4f
-    db  'r',0x4f
-    db  't',0x4f
-    db  'e',0x4f
-    db  'd',0x4f
-    db  '.',0x4f
+  ; dw 0x4f52と書くがエンディアンで分けるときは逆に書く
+  db  'E', 0x4f;4f:赤字に白文字
+  db  'r', 0x4f
+  db  'r', 0x4f
+  db  'o', 0x4f
+  db  'r', 0x4f
+  db  ':', 0x4f
+  db  'M', 0x4f
+  db  'u', 0x4f
+  db  'l', 0x4f
+  db  't', 0x4f
+  db  'i', 0x4f
+  db  'b', 0x4f
+  db  'o', 0x4f
+  db  'o', 0x4f
+  db  't', 0x4f
+  db  ' ', 0x4f
+  db  'i', 0x4f
+  db  's', 0x4f
+  db  ' ', 0x4f
+  db  'n', 0x4f
+  db  'o', 0x4f
+  db  't', 0x4f
+  db  ' ', 0x4f
+  db  's', 0x4f
+  db  'u', 0x4f
+  db  'p', 0x4f
+  db  'p', 0x4f
+  db  'o', 0x4f
+  db  'r', 0x4f
+  db  't', 0x4f
+  db  'e', 0x4f
+  db  'd', 0x4f
+  db  '.', 0x4f
 .end:
 
-
-;スタック
+; スタック
 section .bss
+
 align   4
+
 stack_bottom:
-resb    stack_size
+  resb  stack_size
 stack_top:
--- src/arch/x86_64/boot/init64.asm
+++ src/arch/x86_64/boot/init64.asm
@@ -1,28 +1,32 @@
-;雑な初期化(init.asmの続き)
-;IDTの設定が終わるまでCLIしたままにする。そうでないと割り込みが入って死ぬ。
+; 雑な初期化(init.asmの続き)
+; IDTの設定が終わるまでCLIしたままにする。そうでないと割り込みが入って死ぬ。
 bits 64
 
-section .text
-
+; GLOBAL,EXTERN
 global init64
 extern boot_main
 
-init64:
-    ;セグメントレジスタ初期化、間違ってもCSはいじるな(FS,GSはマルチスレッドで使用する可能性がある...らしい)
-    xor rax,rax
-    mov es,ax
-    mov ss,ax
-    mov ds,ax
-    mov fs,ax
-    mov gs,ax
-    pop rsi;RDI=>RSI=> RDX=>RCX=>R8=>...=>R9が引数リスト
-    pop rdi;32bitでpushして64bitでpopはまずい気がする。
-    mov rsp,stack_top;スタック設定
-    jmp boot_main;//各アーキのbootに入ってる
 
-;スタック
+section .text
+
+init64:
+  ; セグメントレジスタ初期化、間違ってもCSはいじるな(FS,GSはマルチスレッドで使用する可能性がある...らしい)
+  xor   rax,  rax
+  mov   es,   rax
+  mov   ss,   ax
+  mov   ds,   ax
+  mov   fs,   ax
+  mov   gs,   ax
+  pop   rsi             ; RDI=>RSI=> RDX=>RCX=>R8=>...=>R9が引数リスト
+  pop   rdi             ; 32bitでpushして64bitでpopはまずい気がする。
+  mov   rsp,  stack_top ; スタック設定
+  jmp   boot_main       ; 各アーキのbootに入ってる
+
+
 section .bss
+
 align   4
+
 stack_bottom:
-resb    4096 * 10
+  resb  4096 * 10
 stack_top:
