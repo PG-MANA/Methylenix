30af0a053e74508550f9e7d4cbda2bd02461b23d
PG_MANA <pg_mana@taprix.org>
2019-07-06 18:05:07 +0900

    [fix] assembly
    SDMなど読みながら間違った解釈などを直した

--- src/arch/x86_64/boot/common.asm
+++ src/arch/x86_64/boot/common.asm
@@ -4,8 +4,8 @@
 IO_MAP_SIZE equ 0xffff
 STACK_SIZE  equ 256
 
-global initial_stack, gdt_main_code, gdtr0
-global tss_definition, tss_address_definition, tss
+global initial_stack, main_code_segment_descriptor, gdtr0
+global tss_descriptor,tss_descriptor_adress, tss
 global pd, pdpt, pml4
 
 section .bss
@@ -34,38 +34,36 @@ section .data
 align   8
 
 gdt:
-  .all:
-    dq    0                       ; GDT云々するとき下位3にセグメント番号がかぶらないため、わざと0エントリを立てる。
+  dummy:
+    dq    0                         ; GDT云々するとき下位3にセグメント番号がかぶらないため、わざと0エントリを立てる。
 
-gdt_main_code: equ $ - gdt
+main_code_segment_descriptor: equ $ - gdt
     ; すべてコードセグメント
     dq    (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53)
 
-tss_definition: equ $ - gdt
-tss_address_definition:
-    dw    tss.end - tss           ; Limit(Low)
-    dw    0                       ; Base(Low)
-    db    0                       ; Base(middle)
-    db    10001001b               ; 64ビットTSS + DPL:0 + P:1
-    db    0                       ; Limit(High)+Granularity
-    db    0                       ; Base(Middle high)
-    dd    0                       ; Base(High)
-    dw    0                       ; 予約
-    dw    0                       ; 予約
+tss_descriptor: equ $ - gdt
+tss_descriptor_adress:
+    dw    (tss.end - tss) & 0xffff  ; Limit(Low)
+    dw    0                         ; Base(Low)
+    db    0                         ; Base(middle)
+    db    10001001b                 ; 64ビットTSS + DPL:0 + P:1
+    db    (tss.end - tss) & 0xff0000; Limit(High)+Granularity
+    db    0                         ; Base(Middle high)
+    dd    0                         ; Base(High)
+    dw    0                         ; 予約
+    dw    0                         ; 予約
 
 gdtr0:
-  dw    $ - gdt - 1
+  dw    $ - gdt - 1                 ; リミット数(すぐ上がGDTなので計算で求めてる)
   dq    gdt
 
 align   4096
 
 tss:
-tss_address: equ $                   ; tss_addressをうまく使えないか
   dd    0
   dd    tss_stack
   dd    0
   times 22                dd    0
   dd    104 << 16
-
-.end:
   times IO_MAP_SIZE / 8   db    0
+.end:
--- src/arch/x86_64/boot/init_long_mode.asm
+++ src/arch/x86_64/boot/init_long_mode.asm
@@ -7,8 +7,8 @@ bits 32
 ; GLOBAL, EXTERN
 global init_long_mode
 extern init_x86_64                                  ; at init_x86_64.asm
-extern gdt_main_code, tss_address_definition, gdtr0 ; at common.asm
-extern tss_definition, tss, pd, pdpt, pml4
+extern main_code_segment_descriptor, tss_descriptor, gdtr0 ; at common.asm
+extern tss_descriptor_adress, tss, pd, pdpt, pml4
 
 section .text
 
@@ -24,13 +24,13 @@ init_long_mode:
 
   ; TSSセグメント情報書き込み
   mov   eax,  tss
-  mov   word [tss_address_definition + 2],  ax
+  mov   word [tss_descriptor_adress + 2],  ax
   shr   eax,  16
-  mov   byte [tss_address_definition + 4],  al
-  mov   byte [tss_address_definition + 7],  ah
+  mov   byte [tss_descriptor_adress + 4],  al
+  mov   byte [tss_descriptor_adress + 7],  ah
 
   push  0                   ; 64bit POPのための準備
-  push  gdt_main_code       ; あとで使う
+  push  main_code_segment_descriptor
   pushfd
   pop   eax
   mov   ecx,  eax           ; 比較用にとっておく
@@ -120,9 +120,9 @@ pml4_setup:
   or    eax,  1 << 31 | 1   ; PGフラグを立てる("|1"は既に32bitになってる場合は不要)
   mov   cr0,  eax           ; これらの初期化で4GBは仮想メモリアドレスと実メモリアドレスが一致しているはず。(ストレートマッピング)
   lgdt  [gdtr0]
-  mov   ax,   tss_definition
+  mov   ax,   tss_descriptor
   ltr   ax                  ; ホントは16bitから32bitになったときはすぐジャンプすべき
-  jmp   gdt_main_code:init_x86_64
+  jmp   main_code_segment_descriptor:init_x86_64
 
 only_x86:                   ; 今の所下と同じ
 cpuid_not_supported:
