ad46510a67dec703969f36d3a8b888f17fc19ff8
PG_MANA <pg_mana@taprix.org>
2020-08-25 22:46:49 +0900

    [add] the document to boot module

--- src/arch/x86_64/boot/mod.rs
+++ src/arch/x86_64/boot/mod.rs
@@ -1,6 +1,8 @@
-/*
- * Asm files for boot
- */
+//! Assembly files for boot
+//! This module is the boot code to jump to the main code by global_asm
+//! Supported boot system:
+//!  * Multiboot2
+//!  * Xen HVM direct boot
 
 global_asm!(include_str!("common.s"));
 global_asm!(include_str!("multiboot_header.s"));
