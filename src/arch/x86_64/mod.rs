b2b965b939dc6217e2258d8b946ed7aa4abe1f84
PG_MANA <pg_mana@taprix.org>
2020-09-02 23:28:07 +0900

    [fix] not showing panic messages on unknown boot

--- src/arch/x86_64/mod.rs
+++ src/arch/x86_64/mod.rs
@@ -273,5 +273,8 @@ pub extern "C" fn directboot_main(
 
 #[no_mangle]
 pub extern "C" fn unknown_boot_main() {
-    panic!("Unknown Boot System!");
+    SerialPortManager::new(0x3F8).sendstr("Unknown Boot System!");
+    loop {
+        unsafe { llvm_asm!("hlt") };
+    }
 }
