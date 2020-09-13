b5320fd26f9b1ac0b0d9169f19b1aea4a0070ca4
PG_MANA <pg_mana@taprix.org>
2020-09-13 23:56:44 +0900

    [add] TSS Manager

--- src/arch/x86_64/interrupt/tss.rs
+++ src/arch/x86_64/interrupt/tss.rs
@@ -4,12 +4,11 @@
 //! Control TSS.
 //! This struct is not used, but in the future, it may be used to set up ist.
 
-use core::mem;
-
+/*
 const IO_MAP_SIZE: usize = 0xFFFF;
 
-#[repr(C)]
-pub struct TSS {
+#[repr(packed)]
+struct TSS {
     reserved_1: u32,
     rsp0_l: u32,
     rsp0_u: u32,
@@ -41,13 +40,58 @@ pub struct TSS {
     //I/O Permission flag (0:Allow, 1:Forbid)
     io_permission_map: [u8; IO_MAP_SIZE / 8],
 }
+*/
 
-impl TSS {
-    pub fn new(rsp0: u64) -> TSS {
-        let mut res: TSS = mem::MaybeUninit::zeroed();
-        res.rsp0_l = (rsp0 & 0xffffffff) as u32;
-        res.rsp0_u = (rsp0 & 0xffffffff00000000 >> 32) as u32;
-        res.res_and_iomap = (mem::size_of::<TSS>() << 16) as u32;
-        res
+pub struct TssManager {
+    tss: usize,
+}
+
+impl TssManager {
+    pub const fn new() -> Self {
+        Self { tss: 0 }
+    }
+
+    pub fn load_current_tss(&mut self) {
+        use crate::arch::target_arch::device::cpu;
+        let mut gdt: u128 = 0;
+        unsafe { cpu::sgdt(&mut gdt) };
+        let gdt_address = ((gdt >> 16) & core::usize::MAX as u128) as usize;
+        let gdt_limit = (gdt & core::u16::MAX as u128) as u16;
+        let tr = unsafe { cpu::store_tr() };
+        if tr >= gdt_limit {
+            self.tss = 0;
+            return;
+        }
+        let tss_descriptor_address = gdt_address + tr as usize;
+        self.tss = unsafe {
+            (*((tss_descriptor_address + 2) as *const u16)) as usize
+                | ((*((tss_descriptor_address + 4) as *const u8) as usize) << 16)
+                | ((*((tss_descriptor_address + 7) as *const u8) as usize) << 24)
+                | ((*((tss_descriptor_address + 8) as *const u32) as usize) << 32)
+        };
+    }
+
+    pub fn set_ist(&self, ist: u8, stack_address: usize) -> bool {
+        if self.tss == 0 || 0 == ist || ist >= 8 {
+            return false;
+        }
+        let target_ist_address = self.tss + 28 + (ist * 8) as usize;
+        unsafe {
+            *(target_ist_address as *mut u32) = (stack_address & 0xffffffff) as u32;
+            *((target_ist_address + 4) as *mut u32) = (stack_address >> 32) as u32;
+        }
+        return true;
+    }
+
+    pub fn set_rsp(&self, rsp: u8, stack_address: usize) -> bool {
+        if self.tss == 0 || rsp >= 3 {
+            return false;
+        }
+        let target_rsp_address = self.tss + 4 + (rsp * 8) as usize;
+        unsafe {
+            *(target_rsp_address as *mut u32) = (stack_address & 0xffffffff) as u32;
+            *((target_rsp_address + 4) as *mut u32) = (stack_address >> 32) as u32;
+        }
+        return true;
     }
 }
