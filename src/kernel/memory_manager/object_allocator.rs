f59e99492ed12f5928d1596a3eeda0bf822432bb
PG-MANA <pg_mana@taprix.org>
2021-09-03 21:20:14 +0900

    [add] IrqSave&Restore and page_cache
    メモリ確保時に割り込みを禁止するようにした。

--- src/kernel/memory_manager/object_allocator.rs
+++ src/kernel/memory_manager/object_allocator.rs
@@ -10,30 +10,59 @@ mod heap_allocator;
 
 use self::heap_allocator::HeapAllocator;
 
+use crate::arch::target_arch::interrupt::InterruptManager;
 use crate::arch::target_arch::paging::PAGE_SIZE;
 
-use crate::kernel::memory_manager::data_type::{MSize, VAddress};
+use crate::kernel::memory_manager::data_type::{MPageOrder, MSize, VAddress};
 use crate::kernel::memory_manager::{MemoryError, MemoryManager, MemoryPermissionFlags};
 use crate::kernel::sync::spin_lock::Mutex;
 
 pub struct ObjectAllocator {
     heap_allocator: HeapAllocator,
+    page_cache: [VAddress; Self::PAGE_CACHE_LEN],
 }
 
 impl ObjectAllocator {
+    const PAGE_CACHE_LEN: usize = 8;
+
     pub const fn new() -> Self {
         Self {
             heap_allocator: HeapAllocator::new(),
+            page_cache: [VAddress::new(0); Self::PAGE_CACHE_LEN],
         }
     }
 
     pub fn init(&mut self, m_manager: &mut MemoryManager) -> bool {
+        let irq = InterruptManager::save_and_disable_local_irq();
         if let Err(e) = self.heap_allocator.init(m_manager) {
+            InterruptManager::restore_local_irq(irq);
             pr_err!("Setting up ObjectAllocator is failed: {:?}", e);
             return false;
         }
-        /* TODO: init cache_allocator */
+        for e in &mut self.page_cache {
+            *e = match m_manager.alloc_pages(MPageOrder::new(0), MemoryPermissionFlags::data()) {
+                Ok(a) => a,
+                Err(e) => {
+                    InterruptManager::restore_local_irq(irq);
+                    pr_err!("Failed to allocate page cache: {:?}", e);
+                    return false;
+                }
+            };
+        }
+        InterruptManager::restore_local_irq(irq);
         return true;
+    }
+
+    fn alloc_page_from_cache(&mut self) -> Result<VAddress, ()> {
+        const INVALID_ADDRESS: VAddress = VAddress::new(0);
+        for e in self.page_cache.iter_mut().rev() {
+            if *e != INVALID_ADDRESS {
+                let a = *e;
+                *e = INVALID_ADDRESS;
+                return Ok(a);
+            }
+        }
+        return Err(());
     }
 
     pub fn alloc(
@@ -49,7 +78,31 @@ impl ObjectAllocator {
                 MemoryPermissionFlags::data(),
             )
         } else {
-            self.heap_allocator.alloc(size, memory_manager)
+            let irq = InterruptManager::save_and_disable_local_irq();
+            match self.heap_allocator.alloc(size) {
+                Ok(a) => {
+                    InterruptManager::restore_local_irq(irq);
+                    Ok(a)
+                }
+                Err(_) => {
+                    let pool = match self.alloc_page_from_cache() {
+                        Ok(a) => a,
+                        Err(_) => memory_manager
+                            .lock()
+                            .unwrap()
+                            .alloc_pages(MPageOrder::new(0), MemoryPermissionFlags::data())?,
+                    };
+                    self.heap_allocator
+                        .add_pool(size, pool, PAGE_SIZE)
+                        .or(Err(MemoryError::InvalidSize))?;
+                    let result = self
+                        .heap_allocator
+                        .alloc(size)
+                        .or(Err(MemoryError::AddressNotAvailable));
+                    InterruptManager::restore_local_irq(irq);
+                    result
+                }
+            }
         }
     }
 
@@ -64,7 +117,9 @@ impl ObjectAllocator {
         } else if size > PAGE_SIZE {
             memory_manager.lock().unwrap().free(address)
         } else {
+            let irq = InterruptManager::save_and_disable_local_irq();
             self.heap_allocator.dealloc(address, size);
+            InterruptManager::restore_local_irq(irq);
             Ok(())
         }
     }
