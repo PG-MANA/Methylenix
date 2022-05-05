964f3981f20e8b9daa283d0c361ce3c68eb12ceb
PG_MANA <pg_mana@taprix.org>
2022-05-06 08:38:23 +0900

    [improve] system call number
    分離した

--- src/kernel/system_call.rs
+++ src/kernel/system_call.rs
@@ -2,13 +2,16 @@
 //! System Call Handler
 //!
 
-use crate::arch::target_arch::device::cpu;
+mod system_call_number;
+
+use system_call_number::*;
 
 use crate::arch::target_arch::context::context_data::ContextData;
+use crate::arch::target_arch::device::cpu;
 
 pub fn system_call_handler(context: &mut ContextData) {
-    match context.get_system_call_arguments(0).unwrap() {
-        0x01 => {
+    match context.get_system_call_arguments(0).unwrap() as SysCallNumber {
+        SYSCALL_EXIT => {
             pr_info!(
                 "SysCall: Exit(Return Code: {:#X})",
                 context.get_system_call_arguments(1).unwrap()
@@ -18,18 +21,16 @@ pub fn system_call_handler(context: &mut ContextData) {
                 unsafe { cpu::halt() };
             }
         }
-        0x04 => {
+        SYSCALL_WRITE => {
             if context.get_system_call_arguments(1).unwrap() == 1 {
-                kprint!(
-                    "{}",
-                    unsafe {
-                        core::str::from_utf8(core::slice::from_raw_parts(
-                            context.get_system_call_arguments(2).unwrap() as *const u8,
-                            context.get_system_call_arguments(3).unwrap() as usize,
-                        ))
-                    }
-                    .unwrap_or("N/１A")
-                );
+                if let Ok(s) = unsafe {
+                    core::str::from_utf8(core::slice::from_raw_parts(
+                        context.get_system_call_arguments(2).unwrap() as *const u8,
+                        context.get_system_call_arguments(3).unwrap() as usize,
+                    ))
+                } {
+                    kprint!("{s}");
+                }
             } else {
                 pr_debug!(
                     "Unknown file descriptor: {}",
